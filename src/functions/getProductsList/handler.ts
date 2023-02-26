f091dba3e84e554ee7032ee8214f43d8e16372ea
Juan Rivera <guillerivera32@gmail.com>
2023-02-25 19:21:20 -0500

    handle errors in getProductsList

--- src/functions/getProductsList/handler.ts
+++ src/functions/getProductsList/handler.ts
@@ -1,14 +1,38 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
-import products from '@libs/mockProducts.json';
+import productsJSON from '@libs/mockProducts.json';
+
+type Products = typeof productsJSON;
+
+const getProducts: () => Promise<Products> = () => Promise.resolve(productsJSON);
 
 
 const getProductsList: ValidatedEventAPIGatewayProxyEvent<unknown> = async (event) => {
-  return formatJSONResponse({
-    event,
-    products,
-  });
+  try {
+    const products = await getProducts();
+
+    if (!products.length) {
+      return formatJSONResponse({
+        statusCode: 404,
+        event,
+        products,
+      });
+    }
+
+    return formatJSONResponse({
+      statusCode: 200,
+      event,
+      products,
+    });
+  } catch (error) {
+    return formatJSONResponse({
+      statusCode: 500,
+      event,
+      error: `Internal server error: ${error}`
+    })
+  }
+
 };
 
 export const main = middyfy(getProductsList);
--- src/libs/api-gateway.ts
+++ src/libs/api-gateway.ts
@@ -4,13 +4,14 @@ import type { FromSchema } from "json-schema-to-ts";
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
 
-export const formatJSONResponse = (response: Record<string, unknown>) => {
+export const formatJSONResponse = (response: { statusCode: number } & Record<string, unknown>) => {
+  const { statusCode, ...rest } = response;
   return {
-    statusCode: 200,
+    statusCode,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
-    body: JSON.stringify(response)
+    body: JSON.stringify(rest)
   }
 }
