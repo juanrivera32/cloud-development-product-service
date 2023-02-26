773d28efaedc711f29cf80f04b0c76ca65c58fed
Juan Rivera <guillerivera32@gmail.com>
2023-02-25 19:35:44 -0500

    handle errors for getProductById

--- src/functions/getProductsById/handler.ts
+++ src/functions/getProductsById/handler.ts
@@ -3,16 +3,41 @@ import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import products from '@libs/mockProducts.json';
 
+type Product = typeof products[0];
 
-const getProductsById: ValidatedEventAPIGatewayProxyEvent<unknown> = async (event) => {
-  const path = event.path;
-  const productId = path.split('/').filter(Boolean)[1];
+const findProduct = (productId: string): Promise<Product> => {
   const product = products.find(product => product.id === productId);
 
-  return formatJSONResponse({
-    event,
-    product
-  });
+  if (!product) return Promise.resolve(undefined);
+  return Promise.resolve(product);
+}
+
+const getProductsById: ValidatedEventAPIGatewayProxyEvent<unknown> = async (event) => {
+  try {
+    const productId = event.path.split('/').filter(Boolean)[1];
+    const product = await findProduct(productId);
+
+    if (!product) {
+      return formatJSONResponse({
+        statusCode: 404,
+        event,
+        message: `Product ${productId} was not found`
+      });
+    }
+    
+    return formatJSONResponse({
+      statusCode: 200,
+      event,
+      product
+    });
+  } catch (error) {
+    return formatJSONResponse({
+      statusCode: 404,
+      event,
+      message: `Internal server error. Try again later: ${error}`,
+    });
+  }
+
 };
 
 export const main = middyfy(getProductsById);
