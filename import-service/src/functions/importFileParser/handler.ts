04c0cfcee88e147af109a06ff1fcb08f6be70e08
Juan Rivera <guillerivera32@gmail.com>
2023-03-15 22:01:28 -0500

    parse file successfully

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -1,21 +1,24 @@
-import { formatJSONResponse, ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+import {
+  formatJSONResponse,
+  ValidatedEventAPIGatewayProxyEvent,
+} from '@libs/api-gateway';
 import middy from '@middy/core';
 import inputOutputLogger from '@middy/input-output-logger';
 import { getObjectFromS3 } from 'src/services/getObjectFromS3';
 
-// @ts-ignore
-const getObjectFromS3Handler: ValidatedEventAPIGatewayProxyEvent<unknown> = async (
-  event
-) => {
+const getObjectFromS3Handler: ValidatedEventAPIGatewayProxyEvent<
+  unknown
+> = async (event) => {
   // @ts-ignore
-  await getObjectFromS3(event.Records[0].s3.object.key);
-  // return url
+  for (const record of event.Records) {
+    await getObjectFromS3(record.s3.object.key);
+    console.log(`Parsed ${record.s3.object.key} successfully`);
+  }
+
   return formatJSONResponse({
     statusCode: 200,
-    response: 'success',
+    response: `success`,
   });
 };
 
-
-export const main = middy(getObjectFromS3Handler).use(inputOutputLogger())
-
+export const main = middy(getObjectFromS3Handler).use(inputOutputLogger());
--- import-service/src/services/getObjectFromS3.ts
+++ import-service/src/services/getObjectFromS3.ts
@@ -14,22 +14,22 @@ const s3 = new AWS.S3();
 const myBucket = process.env.IMPORT_BUCKET_NAME;
 
 export const getObjectFromS3 = async (objectKey) => {
-  console.log('ObjectKey', objectKey);
+  try {
+    const s3ResponseStream = s3
+      .getObject({ Bucket: myBucket, Key: objectKey })
+      .createReadStream();
 
-  const s3ResponseStream = s3
-    .getObject({ Bucket: myBucket, Key: objectKey }).createReadStream()
+    console.log(`>>>>>> Starting parsing process for ${objectKey}...`);
 
-  console.log('s3ResponseStream', s3ResponseStream)
+    const data = s3ResponseStream.pipe(csvParser({ separator: ';' }));
+    const chunks = [];
+    for await (const chunk of data) {
+      chunks.push(chunk);
+      console.log('chunk ---- ', JSON.stringify(chunk));
+    }
 
-  const chunks = [];
-
-  s3ResponseStream
-    .pipe(csvParser())
-    .on('data', async (data) => {
-      console.log('hi, this is a chunk: ', data);
-      chunks.push(data);
-    })
-    .on('end', () => console.log('logged successfully', chunks))
-    .on('error', (e) => console.log('error reading: ', e))
-    .on('readable', (r) => console.log('readable: ', r));
+    return chunks;
+  } catch (e) {
+    console.log(e);
+  }
 };
