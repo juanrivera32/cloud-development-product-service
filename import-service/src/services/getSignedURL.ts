93f8bed36863e3f677bdf39185826079211b7ce7
Juan Rivera <guillerivera32@gmail.com>
2023-04-05 00:47:02 -0500

    remove credentials from config

--- import-service/src/services/getSignedURL.ts
+++ import-service/src/services/getSignedURL.ts
@@ -2,10 +2,6 @@ import AWS from 'aws-sdk';
 
 AWS.config.update({
   region: 'us-east-1',
-  credentials: {
-    accessKeyId: process.env.ACCESS_KEY,
-    secretAccessKey: process.env.SECRET_ACCESS,
-  },
 });
 
 const s3 = new AWS.S3();
